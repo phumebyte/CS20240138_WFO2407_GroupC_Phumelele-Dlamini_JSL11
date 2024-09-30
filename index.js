9251f2aa4cfccb25115197e193502dc883b89b6c
phumebyte <phumelele.dlamini1000@gmail.com>
2024-09-30 11:48:18 +0200

    Changed forEach loop spelling and added classList to active styling

--- index.js
+++ index.js
@@ -102,13 +102,13 @@ function refreshTasksUI() {
 // Styles the active board by adding an active class
 // TASK: Fix Bugs
 function styleActiveBoard(boardName) {
-  document.querySelectorAll('.board-btn').foreach(btn => { 
+  document.querySelectorAll('.board-btn').forEach(btn => { 
     
     if(btn.textContent === boardName) {
-      btn.add('active') 
+      btn.classList.add('active') 
     }
     else {
-      btn.remove('active'); 
+      btn.classList.remove.('active'); 
     }
   });
 }
