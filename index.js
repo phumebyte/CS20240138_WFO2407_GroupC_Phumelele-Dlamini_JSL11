d4c60882499174137a6e4419ed72c58334acf47e
phumebyte <phumelele.dlamini1000@gmail.com>
2024-09-30 11:41:20 +0200

    Corrected activeBoard ternary syntax

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function fetchAndDisplayBoardsAndTasks() {
   displayBoards(boards);
   if (boards.length > 0) {
     const localStorageBoard = JSON.parse(localStorage.getItem("activeBoard"))
-    activeBoard = localStorageBoard ? localStorageBoard ;  boards[0]; 
+    activeBoard = localStorageBoard ? localStorageBoard :  boards[0]; 
     elements.headerBoardName.textContent = activeBoard
     styleActiveBoard(activeBoard)
     refreshTasksUI();
